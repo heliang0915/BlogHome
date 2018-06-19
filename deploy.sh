3b3fa9124c272cc607225f0b4c6bdc7ab51c3c32
heliang <heliang@duyao001.com>
2018-06-19 18:41:00 +0800

    update

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+cp -R /Users/hotread/work/learn/BlogHome/.nuxt/* /Users/hotread/work/learn/BlogSimple/.nuxt
+cd /Users/hotread/work/learn/BlogSimple
+git pull git@github.com:heliang0915/BlogSimple.git
+git add .
+git commit -m "提交"
+git push  git@github.com:heliang0915/BlogSimple.git
