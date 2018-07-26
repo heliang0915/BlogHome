3b0ce36efc87254d9a29f009a17b4891477b5bf5
heliang <heliang@duyao001.com>
2018-07-26 17:55:15 +0800

    add

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 
 let config={
     api:{
-      baseURL: 'https://blogapi.top'
+      // baseURL: 'https://blogapi.top'
       // ,
-      // baseURL: 'http://localhost:8080'
+      baseURL: 'http://localhost:8080'
     }
 }
 module.exports= config;
