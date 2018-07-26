44ce82098d267f8ebb96247c91b694caa8f0f094
heliang <heliang@duyao001.com>
2018-07-26 17:52:28 +0800

    add

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,11 +1,10 @@
 <template>
   <div class="container">
     <h1 v-if="error.statusCode === 404">页面不存在</h1>
-    <h1 v-else>应用发生错误异常</h1>
+    <h1 v-else>应用发生错误异常 {{error}}</h1>
     <nuxt-link to="/1">首 页</nuxt-link>
   </div>
 </template>
-
 <script>
   export default {
     props: ['error'],
