6763e0be4107cedc0eece8232f8a4d6b76014298
heiang <598481337@qq.com>
2018-07-29 23:31:38 +0800

    提交

--- config.js
+++ config.js
@@ -4,8 +4,8 @@ let config={
       // baseURL:'https://bloghome.top',
       // baseURL:'http://blhome.3w.net579.com',
       baseURL:'http://127.0.0.1:5000',
-      proxyBaseURL: 'https://blogapi.top'// ,
-      // proxyBaseURL: 'http://127.0.0.1:8080'// ,
+      // proxyBaseURL: 'https://blogapi.top'// ,
+      proxyBaseURL: 'http://127.0.0.1:8080'// ,
       // baseURL: 'http://172.17.113.111:8080'
     }
 }
