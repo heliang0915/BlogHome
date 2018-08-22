0c1e6322e5daa54c15ea26692c1fa94300ee7b57
heliang <heliang@duyao001.com>
2018-08-22 10:31:20 +0800

    add

--- pages/layout/blog-header.vue
+++ pages/layout/blog-header.vue
@@ -48,13 +48,19 @@
       },
 
       mounted(){
+        let _this=this;
         setTimeout(()=>{
           this.showClass=true;
           //
         })
-        setTimeout(()=>{
-          this.bg='https://www.pimage.top/cf9d8f85368d2d81309c46595dbf2090?w=2000&q=30';
-        },3000)
+        var image=new Image();
+        image.src="https://www.pimage.top/cf9d8f85368d2d81309c46595dbf2090?w=2000&q=30";
+        image.onload=function(){
+            _this.bg=this.src;
+        }
+        // setTimeout(()=>{
+        //   this.bg='https://www.pimage.top/cf9d8f85368d2d81309c46595dbf2090?w=2000&q=30';
+        // },3000)
       },
       methods:{
          offset(elem){
