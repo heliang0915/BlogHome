cf1ab2c113086a5bc6602f9ffb5868f7ba1275eb
heiang <598481337@qq.com>
2018-08-22 23:28:50 +0800

    update

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,25 +1,3 @@
 <template>
   <nuxt/>
 </template>
-<script>
-   // import api from '../assets/api/api';
-   // import axios from 'axios';
-   // export default {
-   //    data(){
-   //      return {
-   //         channels:[]
-   //      }
-   //    },
-   //     asyncData ({ params }) {
-   //     console.log("asyncData....default");
-   //     // let { data } = await axios.get(`https://jsonplaceholder.typicode.com/users/1`)
-   //     // return { title: data.name }
-   //   },
-   //    mounted(){
-   //      // api .indexQuery.getChannelData().then((data)=>{
-   //      //     console.log(data);
-   //      //     this.channels=data.models;
-   //      // })
-   //    }
-   // }
-</script>
