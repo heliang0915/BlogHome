61fde2ce89d96cd0b7610067ebeae1498ae6835c
heliang <heliang@duyao001.com>
2018-08-23 19:26:54 +0800

    update

--- config.js
+++ config.js
@@ -7,6 +7,6 @@ let config={
       // proxyBaseURL: 'http://www.anhr.top:8080/'// ,
       proxyBaseURL: 'http://127.0.0.1:8080'// ,
     },
-    staticUrl:"//static.blogapi.top/assets/"
+    staticUrl:"//static.blogapi.top/assets"
 }
 module.exports= config;
