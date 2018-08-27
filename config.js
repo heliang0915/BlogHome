25c268297243544cca84876f7f842224e0367bfe
heliang <heliang@duyao001.com>
2018-08-27 12:22:06 +0800

    拆分

--- config.js
+++ config.js
@@ -4,8 +4,8 @@ let config={
       // baseURL:'https://bloghome.top',
       // baseURL:'http://blhome.3w.net579.com',
       baseURL:'http://127.0.0.1:5000',
-      proxyBaseURL: 'http://www.blogapi.top'// ,
-      // proxyBaseURL: 'http://127.0.0.1:8080'// ,
+      // proxyBaseURL: 'http://www.blogapi.top'// ,
+      proxyBaseURL: 'http://127.0.0.1:8080'// ,
     },
     staticUrl:"//static.blogapi.top/assets"
 }
