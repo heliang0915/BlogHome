47688d4f253aa2b4287e4bffa2a011cb12c65ec3
heliang@duyao001.com <598481337@qq.com>
2018-12-05 11:39:41 +0800

    更新

--- pages/layout/blog-footer.vue
+++ pages/layout/blog-footer.vue
@@ -9,12 +9,12 @@
     export default {
        mounted(){
          var _hmt = _hmt || [];
-	(function() {
-           var hm = document.createElement("script");
-           hm.src = "https://hm.baidu.com/hm.js?9c4d21245f10f5a51f94bf71da71d7c2";
-           var s = document.getElementsByTagName("script")[0]; 
- 	   s.parentNode.insertBefore(hm, s);
-	})();
+        (function() {
+                 var hm = document.createElement("script");
+                 hm.src = "https://hm.baidu.com/hm.js?9c4d21245f10f5a51f94bf71da71d7c2";
+                 var s = document.getElementsByTagName("script")[0];
+           s.parentNode.insertBefore(hm, s);
+        })();
 	// var tip= document.querySelector('#tip')
         //  setTimeout(function(){
         //    tip.className="tip-center move-top"
