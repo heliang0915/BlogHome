514f774a366c0de8e8f8261913c5077f9c859d76
heliang@duyao001.com <598481337@qq.com>
2018-12-05 12:13:30 +0800

    优化

--- pages/layout/blog-footer.vue
+++ pages/layout/blog-footer.vue
@@ -8,6 +8,7 @@
 <script>
     export default {
        mounted(){
+         //百度统计
          var _hmt = _hmt || [];
         (function() {
                  var hm = document.createElement("script");
@@ -15,6 +16,19 @@
                  var s = document.getElementsByTagName("script")[0];
            s.parentNode.insertBefore(hm, s);
         })();
+       //百度自动推送
+       (function(){
+         var bp = document.createElement('script');
+         var curProtocol = window.location.protocol.split(':')[0];
+         if (curProtocol === 'https'){
+           bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';
+         }
+         else{
+           bp.src = 'http://push.zhanzhang.baidu.com/push.js';
+         }
+         var s = document.getElementsByTagName("script")[0];
+         s.parentNode.insertBefore(bp, s);
+       })();
 	// var tip= document.querySelector('#tip')
         //  setTimeout(function(){
         //    tip.className="tip-center move-top"
