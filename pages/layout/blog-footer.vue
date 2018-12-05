35270020f4897d81dd8863fa93a1c29eee74e540
heliang@duyao001.com <598481337@qq.com>
2018-12-05 12:28:44 +0800

    优化

--- pages/layout/blog-footer.vue
+++ pages/layout/blog-footer.vue
@@ -17,18 +17,18 @@
            s.parentNode.insertBefore(hm, s);
         })();
        //百度自动推送
-       (function(){
-         var bp = document.createElement('script');
-         var curProtocol = window.location.protocol.split(':')[0];
-         if (curProtocol === 'https'){
-           bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';
-         }
-         else{
-           bp.src = 'http://push.zhanzhang.baidu.com/push.js';
-         }
-         var s = document.getElementsByTagName("script")[0];
-         s.parentNode.insertBefore(bp, s);
-       })();
+       // (function(){
+       //   var bp = document.createElement('script');
+       //   var curProtocol = window.location.protocol.split(':')[0];
+       //   if (curProtocol === 'https'){
+       //     bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';
+       //   }
+       //   else{
+       //     bp.src = 'http://push.zhanzhang.baidu.com/push.js';
+       //   }
+       //   var s = document.getElementsByTagName("script")[0];
+       //   s.parentNode.insertBefore(bp, s);
+       // })();
 	// var tip= document.querySelector('#tip')
         //  setTimeout(function(){
         //    tip.className="tip-center move-top"
